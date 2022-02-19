61473fcb4dac93d20635e6bbcdeb21382cb34f33
weiyuhang1 <49320019+weiyuhang1@users.noreply.github.com>
2022-02-19 00:11:46 -0700

    Time: 144 ms (76.80%), Space: 61.5 MB (50.97%) - LeetHub

---
+++ 904-fruit-into-baskets/904-fruit-into-baskets.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        int result = 0;
+        int sublength = 0;
+        unordered_map<int, int> count;
+        for (int i = 0, j = 0; j < fruits.size(); ++j) {
+            count[fruits[j]]++;
+            while (count.size() > 2) {
+                count[fruits[i]]--;
+                if  (count[fruits[i]] == 0)
+                    count.erase(fruits[i]);
+                i++;
+            }
+            sublength = j - i + 1;
+            result = sublength > result ? sublength : result;
+        }
+        return result;
+    }
+};
