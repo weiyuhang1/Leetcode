3c5feaa6c7125e49a26ab4f2d4e798bff1c2de5f
weiyuhang1 <49320019+weiyuhang1@users.noreply.github.com>
2022-03-08 22:57:11 -0700

    Time: 3 ms (98.97%), Space: 7.3 MB (78.52%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int hash[26] = {};
+        for (int i = 0; i < s.length(); i++){
+            hash[s[i] - 'a']++;
+        }
+        for (int i = 0; i < t.length(); i++){
+            hash[t[i] - 'a']--;
+        }
+        for (int i = 0; i < 26; i++){
+            if (hash[i] != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
