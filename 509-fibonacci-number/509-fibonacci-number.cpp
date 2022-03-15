8d073cf1c86903091f5f78d70dd6a7eba04cd188
weiyuhang1 <49320019+weiyuhang1@users.noreply.github.com>
2022-03-14 22:54:51 -0700

    Time: 0 ms (100.00%), Space: 6 MB (17.13%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int fib(int n) {
+        if (n < 2) return n;
+        vector<int> F(n + 1);
+        F[0] = 0;
+        F[1] = 1;
+        for (int i = 2; i <= n; i++){
+            F[i] = F[i - 1] + F[i - 2];
+        }
+        return F[n];
+        
+        
+        
+        
+    }
+};
