a2aa0182d65a79670e32c80c6a96284142289874
weiyuhang1 <49320019+weiyuhang1@users.noreply.github.com>
2022-03-15 10:58:33 -0700

    Time: 8 ms (55.23%), Space: 8.5 MB (44.43%) - LeetHub

---
+++ 1049-last-stone-weight-ii/1049-last-stone-weight-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int lastStoneWeightII(vector<int>& stones) {
+        int sum = 0;
+        for (int stone : stones){
+            sum += stone;
+        }
+        int target = sum / 2;
+        vector<int> dp(target + 1, 0);
+        for (int stone : stones){
+            for (int j = target; j >= stone; j--){
+                dp[j] = max(dp[j], dp[j - stone] + stone);
+            }
+        }
+        return abs((sum - 2 * dp[target]));
+    }
+};
