af801eb53a01b874ae73d583911c97741bdc5742
weiyuhang1 <49320019+weiyuhang1@users.noreply.github.com>
2022-04-30 10:34:09 -0700

    Time: 4 ms (27.13%), Space: 6.4 MB (53.10%) - LeetHub

---
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<int> row1 = {1};
+        if (rowIndex == 0) return row1;
+        
+        
+        
+        for (int i = 1; i <= rowIndex; i++) {
+            vector<int> row2(i + 1, 1);
+            for (int j = 1; j < i; j++) {
+                row2[j] = row1[j - 1] + row1[j];
+            }
+            row1 = row2;
+            
+        }
+        return row1;
+        
+        
+    }
+};
