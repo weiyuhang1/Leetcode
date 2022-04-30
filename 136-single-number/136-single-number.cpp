da7c1cefa6edb0398e1a5b430d27658c43ef0dd7
weiyuhang1 <49320019+weiyuhang1@users.noreply.github.com>
2022-04-30 13:47:03 -0700

    Time: 15 ms (94.58%), Space: 16.9 MB (86.93%) - LeetHub

---
+++ 136-single-number/136-single-number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int sum = 0;
+        for (auto i : nums) {
+            sum ^= i;
+        }
+        return sum;
+    }
+};
