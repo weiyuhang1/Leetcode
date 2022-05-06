e8cf5d53244ace9392ecebbed0e01b0ae609ee1a
weiyuhang1 <49320019+weiyuhang1@users.noreply.github.com>
2022-05-06 12:17:17 -0700

    Time: 0 ms (100.00%), Space: 10.2 MB (6.29%) - LeetHub

--- 226-invert-binary-tree/226-invert-binary-tree.cpp
+++ 226-invert-binary-tree/226-invert-binary-tree.cpp
@@ -16,10 +16,10 @@ public:
 				return NULL;
 			TreeNode* temp = new TreeNode(root->val);// create node with (NULL, val, NULL)
 
-			if(root->right)//if root->right is Not NULL, it will assign it to temp->left : else let it be NULL 
+		//	if(root->right)//if root->right is Not NULL, it will assign it to temp->left : else let it be NULL 
 				temp-> left = invertTree(root->right);
 
-			if(root->left)// same as before
+	//		if(root->left)// same as before
 				temp-> right = invertTree(root->left); 
 
 			return temp;//return temp node
