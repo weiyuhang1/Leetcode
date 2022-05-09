c7e60c6abff4bc329654890aff2f6dd425adad38
weiyuhang1 <49320019+weiyuhang1@users.noreply.github.com>
2022-05-09 12:00:14 -0700

    Time: 12 ms (69.45%), Space: 20.9 MB (34.47%) - LeetHub

---
+++ 270-closest-binary-search-tree-value/270-closest-binary-search-tree-value.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+ 
+    int closestValue(TreeNode* root, double target) {
+
+   //     if (root->left == nullptr && root->right == nullptr) return root->val;
+        if (target > root->val) {
+            if (root->right)
+                return target - root->val < abs(closestValue(root->right, target) - target) ? root->val : closestValue(root->right, target);
+        } else {
+            if (root->left)
+                return root->val - target < abs(closestValue(root->left, target) - target) ? root-> val : closestValue(root->left, target);
+        }
+        return root->val;
+
+    }
+    
+};
