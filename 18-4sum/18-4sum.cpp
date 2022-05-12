4a153d26ce1354acebe135dde24fcda18cdfebf2
weiyuhang1 <49320019+weiyuhang1@users.noreply.github.com>
2022-05-12 11:14:46 -0700

    Time: 99 ms (48.77%), Space: 13.2 MB (50.20%) - LeetHub

--- 18-4sum/18-4sum.cpp
+++ 18-4sum/18-4sum.cpp
@@ -3,22 +3,24 @@ public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         vector<vector<int>> result;
         sort(nums.begin(), nums.end());
+        int l, r, sum, subtarget;
         for (int i = 0; i < nums.size(); i++) {
             if (i != 0 && nums[i] == nums[i - 1]) continue;
             for (int j = i + 1; j < nums.size(); j++) {
                 if (j != i + 1 && nums[j] == nums[j - 1]) continue;
-                int l = j + 1;
-                int r = nums.size() - 1;
+                l = j + 1;
+                r = nums.size() - 1;
                 while (l < r) {
-                    int sum = target - nums[i] - nums[j];
-                    if (sum == nums[l] + nums[r]) {
+                    subtarget = target - nums[i] - nums[j];
+                    sum = nums[l] + nums[r];
+                    if (sum == subtarget) {
                         result.push_back({nums[i], nums[j], nums[l], nums[r]});
                         l++;
                         while (l < r && nums[l] == nums[l - 1])
-                            l++;
-                    } else if (sum > nums[l] + nums[r]) {
                         l++;
-                    } else if (sum < nums[l] + nums[r]) {
+                    } else if (sum < subtarget) {
+                        l++;
+                    } else {
                         r--;
                     }
                     
