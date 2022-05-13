c3c684b3fc8644099ea7901249632ff8cfb0408e
weiyuhang1 <49320019+weiyuhang1@users.noreply.github.com>
2022-05-13 15:33:32 -0700

    Time: 18 ms (90.88%), Space: 10.1 MB (50.33%) - LeetHub

--- 77-combinations/77-combinations.cpp
+++ 77-combinations/77-combinations.cpp
@@ -15,7 +15,7 @@ private:
             return;
                 
         }
-        for (int i = startindex; i <= n; i++) {
+        for (int i = startindex; i <= n - (k - path.size()) +1; i++) {
             path.push_back(i);
             backtracking(n, k, i + 1);
             path.pop_back();
