5179793cb19fa0cb6b2b9526ad4c9383df4ac8e8
weiyuhang1 <49320019+weiyuhang1@users.noreply.github.com>
2022-05-13 16:02:01 -0700

    Time: 0 ms (100.00%), Space: 6.3 MB (86.39%) - LeetHub

---
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum3(int k, int n) {
+        backtracking(k, n, 0, 1);
+        return result;
+        
+    }
+private:
+    vector<vector<int>> result;
+    vector<int> path;
+    void backtracking(int k, int n, int sum, int startindex) {
+        if (path.size() == k) {
+            if (sum == n) result.push_back(path);
+            return;
+        }
+        for (int i = startindex; i <= 9; i++) {
+            path.push_back(i);
+            sum += i;
+            backtracking(k, n, sum, i + 1);
+            sum -= i;
+            path.pop_back();
+        }
+        
+        
+    }
+    
+};
