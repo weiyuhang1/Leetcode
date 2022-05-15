772613cdb991c6fe1148cf7bfd44af186ff57c2d
weiyuhang1 <49320019+weiyuhang1@users.noreply.github.com>
2022-05-15 13:43:47 -0700

    Time: 0 ms (100.00%), Space: 11.6 MB (73.19%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        int l = n;
+        int r = n;
+        backtracking(l, r);
+        return result;
+        
+    }
+private:
+    vector<string> result;
+    string path;
+    void backtracking(int&l, int& r) {
+        if (l == 0 && r == 0) {
+            result.push_back(path);
+            return;
+        }
+        if (l > 0) {
+            path.push_back('(');
+            l--;
+            backtracking(l, r);
+            path.pop_back();
+            l++;
+        }
+        
+        if (r > l) {
+            path.push_back(')');
+            r--;
+            backtracking(l, r);
+            path.pop_back();
+            r++;
+        }
+        
+    
+        
+    }
+    
+};
