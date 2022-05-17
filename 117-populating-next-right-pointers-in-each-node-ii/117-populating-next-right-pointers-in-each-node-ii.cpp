0d9e523d457b3c607d570c754b2d027a0af3f9b2
weiyuhang1 <49320019+weiyuhang1@users.noreply.github.com>
2022-05-16 21:51:54 -0700

    Time: 13 ms (82.26%), Space: 17.4 MB (84.29%) - LeetHub

---
+++ 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.cpp
@@ -0,0 +1,49 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if (root == nullptr) return root;
+        Node* leftmost = root;
+        Node* cur;
+        while (leftmost) {
+            cur = leftmost;
+            leftmost = nullptr;
+            Node* pre = nullptr;
+            while (cur) {
+                processChild(cur->left, pre, leftmost);
+                processChild(cur->right, pre, leftmost);
+                cur = cur->next;
+            }
+        }
+        return root;
+    }
+private:
+    void processChild(Node* child, Node*& pre, Node*& leftmost) {
+        if (child == nullptr) return;
+        if (pre == nullptr) {
+            pre = child;
+            leftmost = child;
+        } else {
+            pre->next = child;
+            pre = child;
+        }
+    }
+    
+};
