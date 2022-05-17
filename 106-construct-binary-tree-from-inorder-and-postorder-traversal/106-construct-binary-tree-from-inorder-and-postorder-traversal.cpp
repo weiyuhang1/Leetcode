7d3e27701d3e0368099071f7909e660d11a713a8
weiyuhang1 <49320019+weiyuhang1@users.noreply.github.com>
2022-05-17 16:30:45 -0700

    Time: 19 ms (79.61%), Space: 26.3 MB (57.38%) - LeetHub

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -12,31 +12,34 @@
 class Solution {
 public:
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        
-        return traversal(inorder, postorder);
+        unordered_map<int, int> mp;
+        for (int i = 0; i< inorder.size(); i++) {
+            mp[inorder[i]] = i;
+            }   
+        return traversal(inorder, postorder, mp, 0, inorder.size(), 0, postorder.size());
         
     }
 private:
-    TreeNode* traversal(vector<int>& inorder, vector<int>& postorder) {
-        if (postorder.empty()) return nullptr;
+    TreeNode* traversal(vector<int>& inorder, vector<int>& postorder, unordered_map<int, int>& mp,
+                        int inorderbegin, int inorderend, int postorderbegin, int postorderend) {
+        if (postorderend == postorderbegin) return nullptr;
         //root node
-        TreeNode* root = new TreeNode(postorder.back());
-        if (postorder.size() == 1) return root;
+        TreeNode* root = new TreeNode(postorder[postorderend - 1]);
+        if (postorderend - postorderbegin == 1) return root;
      //   postorder.resize(postorder.size() - 1);
-        int index;
-        for (int i = 0; i< inorder.size(); i++) {
-            if (inorder[i] == root->val) {
-                index = i;
-                break;
-            }
-        }
-        vector<int> leftinorder(inorder.begin(), inorder.begin() + index);
-        vector<int> rightinorder(inorder.begin() + index + 1, inorder.end());
+        int index = mp[root->val];
+        int leftinorderbegin = inorderbegin;
+        int leftinorderend = index;
+        int rightinorderbegin = index + 1;
+        int rightinorderend = inorderend;
         
-        vector<int> leftpostorder(postorder.begin(), postorder.begin() + leftinorder.size());
-        vector<int> rightpostorder(postorder.begin() + leftinorder.size(), postorder.end() - 1);
-        root->left = buildTree(leftinorder, leftpostorder);
-        root->right = buildTree(rightinorder, rightpostorder);
+        int leftpostorderbegin = postorderbegin;
+        int leftpostorderend = postorderbegin + leftinorderend - leftinorderbegin;
+        int rightpostorderbegin = leftpostorderend;
+        int rightpostorderend = postorderend - 1;
+        
+        root->left = traversal(inorder, postorder, mp, leftinorderbegin, leftinorderend, leftpostorderbegin, leftpostorderend);
+        root->right = traversal(inorder, postorder, mp, rightinorderbegin, rightinorderend, rightpostorderbegin, rightpostorderend);
         return root;
     }
     
