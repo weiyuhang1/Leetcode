fb26f0b84896a3e272660bea72e9be787eae784d
weiyuhang1 <49320019+weiyuhang1@users.noreply.github.com>
2022-05-21 11:04:38 -0700

    Time: 191 ms (57.33%), Space: 13.7 MB (92.84%) - LeetHub

--- 127-word-ladder/127-word-ladder.cpp
+++ 127-word-ladder/127-word-ladder.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
         unordered_set s(wordList.begin(), wordList.end());
-        vector<bool> visited(wordList.size(), false);
         queue<string> q;
         q.push(beginWord);
         int level = 1;
@@ -10,8 +9,6 @@ public:
             int size = q.size();
             level++;
             for (int j = 0; j < size; j++) {
-                
-                
                 for (int i = 0; i < q.front().size(); i++) {
                     string currentword = q.front();
                     for (int j = 0; j < 26; j++) {
@@ -21,36 +18,11 @@ public:
                             s.erase(currentword);
                             q.push(currentword);
                         }
-                            
-                        
                     }
                 }
                 q.pop();
-                
-                
-                /*
-                for (int i = 0; i < wordList.size(); i++) {
-                    if (visited[i] == false && ValidNextWord(currentword, wordList[i])) {
-                        if (wordList[i] == endWord) return level;
-                        visited[i] = true;
-                        q.push(wordList[i]);
-                    }
-                }
-                */
-            }
-            
+            }  
         }
-        return 0;
-        
+        return 0;    
     }
-private:
-    bool ValidNextWord (string &currentword, string& nextword) {
-        if (currentword.size() != nextword.size()) return false;
-        int diff = 0;
-        for (int i = 0; i < currentword.size(); i++) {
-            if (currentword[i] != nextword[i]) diff++;
-        }
-        return diff == 1;
-    }
-    
 };
