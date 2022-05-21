65bf6ccf3f2a67f02cebe3d62692cf25df85696f
weiyuhang1 <49320019+weiyuhang1@users.noreply.github.com>
2022-05-20 18:02:43 -0700

    Time: 23 ms (16.25%), Space: 8.9 MB (70.39%) - LeetHub

---
+++ 684-redundant-connection/684-redundant-connection.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n = edges.size();
+        parent.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+        }
+        for (vector<int> edge : edges) {
+            if (sameSet(edge[0], edge[1])) {
+                return edge;
+            } else {
+                unionSet(edge[0], edge[1]);
+            }
+        }
+        return edges[0];
+    }
+private:
+    vector<int> parent;
+    int find(int i) {
+        while (parent[i] != i) {
+            i = parent[i];
+        }
+        return i;
+    }
+    bool sameSet(int u, int v) {
+        return find(u) == find(v);
+    }
+    void unionSet(int u, int v) {
+        parent[find(u)] = parent[v];
+    }
+    
+    
+    
+    
+};
