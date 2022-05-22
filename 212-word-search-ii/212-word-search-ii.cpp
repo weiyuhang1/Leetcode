b6154ca9e9d1e5fd3dbceabe597e31b124757f14
weiyuhang1 <49320019+weiyuhang1@users.noreply.github.com>
2022-05-21 23:09:22 -0700

    Time: 1131 ms (36.86%), Space: 12.7 MB (90.65%) - LeetHub

--- 212-word-search-ii/212-word-search-ii.cpp
+++ 212-word-search-ii/212-word-search-ii.cpp
@@ -10,18 +10,15 @@ public:
     }
 };
 
-
-
 class Solution {
 public:
     vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
-        vector<vector<bool>> used(board.size(), vector<bool>(board[0].size(), false));
         root = new TrieNode();
         for (string& word : words)
             insert(root, word);
         for (int i = 0; i < board.size(); i++) {
             for (int j = 0; j < board[0].size(); j++) {
-                backtracking(board, i, j, used, root);
+                backtracking(board, i, j, root);
             }
         }
         return result;
@@ -30,23 +27,25 @@ private:
     TrieNode* root;
     string path;
     vector<string> result;
-    void backtracking(vector<vector<char>>& board, int i, int j, vector<vector<bool>>& used, TrieNode* root) {
-        if (i < 0 || i >= board.size() || j < 0 || j >= board[0].size() || used[i][j] == true 
+    void backtracking(vector<vector<char>>& board, int i, int j, TrieNode* root) {
+        if (i < 0 || i >= board.size() || j < 0 || j >= board[0].size() || board[i][j] == '#' 
            || root->children[board[i][j] - 'a'] == nullptr) return;
         path += board[i][j];
-        used[i][j] = true;
+        char c = board[i][j];
+        
         root = root->children[board[i][j] - 'a'];
+        board[i][j] = '#';
         if (root->isEnd == true) {
             result.push_back(path); //needs path -=
             root->isEnd = false;
         }
         
-        backtracking(board, i - 1, j, used, root);
-        backtracking(board, i + 1, j, used, root);
-        backtracking(board, i, j - 1, used, root);
-        backtracking(board, i, j + 1, used, root);
+        backtracking(board, i - 1, j, root);
+        backtracking(board, i + 1, j, root);
+        backtracking(board, i, j - 1, root);
+        backtracking(board, i, j + 1, root);
         path.pop_back();
-        used[i][j] = false;
+        board[i][j] = c;
     }
     void insert(TrieNode* root, string& word) {
         TrieNode* cur = root;
