8c32b3acfa7c8e20b8817564f2f29ba6e95a353d
weiyuhang1 <49320019+weiyuhang1@users.noreply.github.com>
2022-05-22 10:57:19 -0700

    Time: 0 ms (100.00%), Space: 6.3 MB (36.29%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -1,25 +1,46 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        int fastindex = 0;
-        int slowindex = 0;
-        int result = -1;
-        int j = 0;
-        while (slowindex < haystack.length()){
-            if (j == needle.length()) return slowindex;
-            
-            
-            
-            if (haystack[fastindex] == needle[j]){
-                fastindex++;
+        vector<int> lps(needle.size(), 0);
+        preprocessKMP(needle, lps);
+        for (int i = 0, j = 0; i < haystack.size(); ) {
+            if (haystack[i] == needle[j]) {
+                i++;
                 j++;
-            }else{
-                slowindex++;
-                fastindex = slowindex;
-
-                j = 0;
+            
+                if (j == needle.size())
+                 return i - j;
+            } else if (haystack[i] != needle[j]) {
+                if (j != 0) {
+                    j = lps[j - 1];
+                } else {
+                    i++;
+                }
+            } 
+        }
+        return -1;
+    }
+private:
+    
+    void preprocessKMP(string& needle, vector<int>& lps) {
+        int len = 0;
+        for (int i = 1; i < lps.size(); ) {
+            if (needle[i] == needle[len]) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len > 0) {
+                    len = lps[len - 1];
+                    
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
             }
         }
-        return result;
+        
     }
+    
+    
 };
