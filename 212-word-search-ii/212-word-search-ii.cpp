cd0f92a5fcb7e389431fa776a2c7f1ebe204cb47
weiyuhang1 <49320019+weiyuhang1@users.noreply.github.com>
2022-05-21 22:52:43 -0700

    Time: 1432 ms (25.14%), Space: 13.4 MB (37.26%) - LeetHub

---
+++ 212-word-search-ii/212-word-search-ii.cpp
@@ -0,0 +1,80 @@
+class TrieNode {
+public:
+    bool isEnd;
+    TrieNode* children[26];
+    TrieNode() {
+        isEnd = false;
+        for (int i = 0; i < 26; i++) {
+            children[i] = nullptr;
+        }
+    }
+};
+
+
+
+class Solution {
+public:
+    vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
+        vector<vector<bool>> used(board.size(), vector<bool>(board[0].size(), false));
+        root = new TrieNode();
+        for (string& word : words)
+            insert(root, word);
+        for (int i = 0; i < board.size(); i++) {
+            for (int j = 0; j < board[0].size(); j++) {
+                backtracking(board, i, j, used, root);
+            }
+        }
+        vector<string> result_vector;
+        for (const string& element : result) {
+            result_vector.push_back(element);
+        }
+        return result_vector;
+    }
+private:
+    TrieNode* root;
+    string path;
+    unordered_set<string> result;
+    void backtracking(vector<vector<char>>& board, int i, int j, vector<vector<bool>>& used, TrieNode* root) {
+        if (i < 0 || i >= board.size() || j < 0 || j >= board[0].size() || used[i][j] == true || root == nullptr
+           || root->children[board[i][j] - 'a'] == nullptr) return;
+        path += board[i][j];
+        used[i][j] = true;
+        root = root->children[board[i][j] - 'a'];
+        if (root->isEnd == true) result.insert(path); //needs path -=
+        
+        backtracking(board, i - 1, j, used, root);
+        backtracking(board, i + 1, j, used, root);
+        backtracking(board, i, j - 1, used, root);
+        backtracking(board, i, j + 1, used, root);
+        path.pop_back();
+        used[i][j] = false;
+    }
+    void insert(TrieNode* root, string& word) {
+        TrieNode* cur = root;
+        for (int i = 0; i < word.size(); i++) {
+            if (cur->children[word[i] - 'a'] == nullptr)
+                cur->children[word[i] - 'a'] = new TrieNode();
+            cur = cur->children[word[i] - 'a'];
+        }
+        cur->isEnd = true;
+    }
+    bool searchPrefix(TrieNode* root, string& prefix) {  
+        TrieNode* cur = root;
+        for (int i = 0; i < prefix.size(); i++) {
+            if (cur->children[prefix[i] - 'a'] == nullptr) return false;
+            cur = cur->children[prefix[i] - 'a'];
+        }
+        return true;
+    }
+    bool searchWord(TrieNode* root, string& word) {  
+        TrieNode* cur = root;
+        for (int i = 0; i < word.size(); i++) {
+            if (cur->children[word[i] - 'a'] == nullptr) return false;
+            cur = cur->children[word[i] - 'a'];
+        }
+        return cur->isEnd;
+    }
+    
+    
+    
+};
