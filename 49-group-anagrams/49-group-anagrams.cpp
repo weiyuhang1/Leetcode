32bce302123c33ca535728ede52a1b882993b86e
weiyuhang1 <49320019+weiyuhang1@users.noreply.github.com>
2022-05-23 12:31:31 -0700

    Time: 51 ms (51.34%), Space: 21.7 MB (23.71%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -1,28 +1,31 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        vector<vector<int>> matrix(strs.size(), vector(27, 0));
         vector<vector<string>> result;
+        unordered_map<string, vector<string>> mp;
         for (int i = 0; i < strs.size(); i++) {
-            for (const char& c : strs[i]) {
-                matrix[i][c - 'a']++;
-            }
-            matrix[i][26] = 1;
+            mp[strSort(strs[i])].push_back(strs[i]);
         }
-        for (int i = 0; i < matrix.size(); i++) {
-            if (matrix[i][26] == 0) continue;
-            vector<string> path{strs[i]};
-            for (int j = i + 1; j < matrix.size(); j++) {
-                if (matrix[j][26] == 0 || strs[j].size() != strs[i].size()) continue;
-                if (matrix[j] == matrix[i]) {
-                    path.push_back(strs[j]);
-                    matrix[j][26] = 0;
-                }
-            }
-            matrix[i][26] = 0;
-            result.push_back(path);
+        
+        for (auto p : mp) {
+            result.push_back(p.second);
         }
+
+
         
         return result;
     }
+private:
+    string strSort(string s) {
+        vector<char> count(26, 0);
+        for (const char& c : s) {
+                count[c - 'a']++;
+        }
+        string result;
+        for (int i = 0; i < count.size(); i++) {
+            result += string(count[i], i + 'a');
+        }
+        return result;
+    }
+    
 };
