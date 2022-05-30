c0270740d683388c818dabc3104e480e6e38a442
weiyuhang1 <49320019+weiyuhang1@users.noreply.github.com>
2022-05-30 00:18:00 -0700

    Time: 151 ms (23.75%), Space: 48.4 MB (44.53%) - LeetHub

---
+++ 55-jump-game/55-jump-game.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        if (nums.size() == 1) return true;
+        int cover = 0;
+        for (int i = 0; i <= cover; i++) {
+            cover = max(i + nums[i], cover);
+            if (cover >= nums.size() - 1) return true;
+        }
+        return false;
+    }
+};
