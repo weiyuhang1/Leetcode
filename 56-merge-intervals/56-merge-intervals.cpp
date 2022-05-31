740938ee294f9b34c9aedaf170b032cd86b5c1f9
weiyuhang1 <49320019+weiyuhang1@users.noreply.github.com>
2022-05-30 22:54:07 -0700

    Time: 79 ms (18.92%), Space: 18.9 MB (94.49%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        if (intervals.size() == 1) return intervals;
+        sort(intervals.begin(), intervals.end(), cmp);
+        vector<vector<int>> result;
+        int end = intervals[0][1];
+        int start = intervals[0][0];
+        for (int i = 1; i < intervals.size(); i++) {
+            if (intervals[i][0] <= end) {
+                
+                end = max(end, intervals[i][1]);
+            } else {
+                result.push_back({start, end});
+                start = intervals[i][0];
+                end = intervals[i][1];
+            }
+        }
+        result.push_back({start, end});
+        return result;
+    }
+private:
+    static bool cmp(vector<int>& a, vector<int>& b) {
+        return a[0] < b[0];
+    }
+    
+};
