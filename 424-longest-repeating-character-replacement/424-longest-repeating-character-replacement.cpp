59c73b4d18de4e6bc440479eef2b0ef1230b45b2
weiyuhang1 <49320019+weiyuhang1@users.noreply.github.com>
2022-06-04 10:04:06 -0700

    Time: 19 ms (46.66%), Space: 7.1 MB (54.63%) - LeetHub

---
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        vector<int> count(26, 0);
+        int result = 0;
+        int start = 0;
+        int maxcount = 0;
+        for (int end = 0; end < s.size(); end++) {
+            count[s[end] - 'A']++;
+            maxcount = max(maxcount, count[s[end] - 'A']);
+            while (end - start + 1 - maxcount > k) {
+              
+                count[s[start] - 'A']--;
+                start++;
+                maxcount = 0;
+                for (int i = 0; i < count.size(); i++) {
+                    if (count[i] > maxcount) maxcount = count[i];
+                }
+                
+                
+            }
+            result = max(result, end - start + 1);
+        }
+        return result;
+    }
+};
