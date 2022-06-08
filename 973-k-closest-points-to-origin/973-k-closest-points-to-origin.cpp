aacc97d02c13c9d0cea551dd98847a032531803d
weiyuhang1 <49320019+weiyuhang1@users.noreply.github.com>
2022-06-08 08:04:05 -0700

    Time: 697 ms (12.47%), Space: 77.6 MB (23.52%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -1,3 +1,8 @@
+    class comp{
+         public:       bool operator()(vector<int>&a, vector<int>& b) {
+                return a[0] * a[0] + a[1] * a[1] <= b[0] * b[0] + b[1] * b[1]; 
+            }
+        };
 class Solution {
      
     public:
@@ -16,13 +21,9 @@ class Solution {
         return result;
     }
 
- public:    
-    struct comp{
-            bool operator()(vector<int>&a, vector<int>& b) {
-                return a[0] * a[0] + a[1] * a[1] <= b[0] * b[0] + b[1] * b[1]; 
-            }
-        };
+
       
 
     
 };
+ 
