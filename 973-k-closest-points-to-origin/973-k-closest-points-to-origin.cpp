c67b505f95bc2bfbba2da1a4d5a04b9b54415cbe
weiyuhang1 <49320019+weiyuhang1@users.noreply.github.com>
2022-06-08 08:19:12 -0700

    Time: 484 ms (25.35%), Space: 77.6 MB (23.52%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -1,9 +1,4 @@
-class comp{
-    public:
-    bool operator()(vector<int>&a, vector<int>& b) {
-        return a[0] * a[0] + a[1] * a[1] <= b[0] * b[0] + b[1] * b[1]; 
-    }
-};
+
 class Solution {     
     public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
@@ -18,6 +13,13 @@ class Solution {
             pq.pop();
         }
         return result;
-    }  
+    }
+private:
+    struct comp{
+        bool operator()(vector<int>& a, vector<int>& b) {
+            return a[0] * a[0] + a[1] * a[1] < b[0] * b[0] + b[1] * b[1];
+        }
+    };
+    
 };
  
