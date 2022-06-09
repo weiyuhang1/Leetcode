346284e0ca748e5c8782b875ea49ef64b9cdc7e8
weiyuhang1 <49320019+weiyuhang1@users.noreply.github.com>
2022-06-08 19:10:22 -0700

    Time: 90 ms (71.99%), Space: 34.5 MB (73.56%) - LeetHub

--- 621-task-scheduler/621-task-scheduler.cpp
+++ 621-task-scheduler/621-task-scheduler.cpp
@@ -7,7 +7,6 @@ public:
         }
         int most = *max_element(begin(mp), end(mp));
         int nmax = count(begin(mp), end(mp), most);
-        int sumothers = accumulate(begin(mp), end(mp), 0) - most;
-        return max((most - 1) * (n + 1) + nmax, sumothers + most);
+        return max((most - 1) * (n + 1) + nmax, (int)tasks.size());
     }
 };
