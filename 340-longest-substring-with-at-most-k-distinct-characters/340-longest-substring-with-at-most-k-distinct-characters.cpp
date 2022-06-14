e4cc8056597e282c757a187edba61f99ac4a78bc
weiyuhang1 <49320019+weiyuhang1@users.noreply.github.com>
2022-06-13 21:05:24 -0700

    Time: 44 ms (6.41%), Space: 8.2 MB (22.74%) - LeetHub

--- 340-longest-substring-with-at-most-k-distinct-characters/340-longest-substring-with-at-most-k-distinct-characters.cpp
+++ 340-longest-substring-with-at-most-k-distinct-characters/340-longest-substring-with-at-most-k-distinct-characters.cpp
@@ -5,27 +5,16 @@ public:
         int maxlen = INT_MIN;
         int i, j;
         for (i = 0, j = 0; j < s.length(); j++) {
-            if (mp.size() < k || mp.find(s[j]) != mp.end()) {
-                mp[s[j]] = j;
-            } else {
-                    char c;
-                    int pos = INT_MAX;
-                    mp[s[j]] = j;
-                    for (auto it : mp) {
-                        if (it.second < pos) {
-                            pos = it.second;
-                            c = it.first;
-                        }
-                    }
-                    i = pos + 1;
-                    mp.erase(c);
-
-           
+            mp[s[j]]++;
+            while (mp.size() > k) {
+                mp[s[i]]--;
+                if (mp[s[i]] == 0) mp.erase(s[i]);
+                i++;
             }
-                                maxlen = max(maxlen, j - i + 1);
+            maxlen = max(maxlen, j - i + 1);
         }
         
-        if (i == 0) maxlen = s.length();
+    //    if (i == 0) maxlen = s.length();
         return maxlen;
     }
 }; 
