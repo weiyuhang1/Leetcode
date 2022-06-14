1e6a44319ba89a7c1f1a1e67896e2d6f8e01d7e0
weiyuhang1 <49320019+weiyuhang1@users.noreply.github.com>
2022-06-13 18:19:25 -0700

    Time: 686 ms (10.53%), Space: 53.9 MB (5.26%) - LeetHub

---
+++ 2067-number-of-equal-count-substrings/2067-number-of-equal-count-substrings.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int equalCountSubstrings(string s, int count) {
+        const int maxunique = unordered_set<char>(s.begin(), s.end()).size();
+        
+        const int n = s.length();
+        int times = 0;
+        for (int uniquechar = 1; uniquechar * count <= n && uniquechar <= maxunique; uniquechar++) {
+            unordered_map<char, int> mp;
+            int len = uniquechar * count;
+            for (int j = 0; j < n; j++) {
+                mp[s[j]]++;
+                if (j > len - 1) {
+                    int i = j - len;
+                    mp[s[i]]--;
+                    if (mp[s[i]] <= 0) mp.erase(s[i]);
+              
+                }
+                if (isValid(mp, count) && mp.size() == uniquechar) times++;
+            
+            }
+            
+            
+        }
+        return times;
+        
+        
+        
+    }
+private:
+    bool isValid(unordered_map<char, int>& mp, int count) {
+        for (auto pair : mp) {
+            if (pair.second != count && pair.second != 0) return false;
+        }
+        return true;
+    }
+    
+    
+};
