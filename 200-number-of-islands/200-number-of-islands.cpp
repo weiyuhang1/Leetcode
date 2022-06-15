05ff79fd7f8265be7117f0c8b28e138676df5be1
weiyuhang1 <49320019+weiyuhang1@users.noreply.github.com>
2022-06-14 18:26:38 -0700

    Time: 46 ms (59.98%), Space: 12.3 MB (58.76%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -17,13 +17,12 @@ public:
 private:
     int count;
     void dfs(vector<vector<char>>& grid, int row, int col) {
-        if (row < 0 || row >= grid.size() || col < 0 || col >= grid[0].size()) return;
         if (grid[row][col] == '0') return;
         grid[row][col] = '0';
-        dfs(grid, row - 1, col);
-        dfs(grid, row + 1, col);
-        dfs(grid, row, col - 1);
-        dfs(grid, row, col + 1);
+        if (row - 1 >= 0) dfs(grid, row - 1, col);
+        if (row + 1 < grid.size()) dfs(grid, row + 1, col);
+        if (col - 1 >= 0) dfs(grid, row, col - 1);
+        if (col + 1 < grid[0].size()) dfs (grid, row, col + 1);
     }
     
 };
