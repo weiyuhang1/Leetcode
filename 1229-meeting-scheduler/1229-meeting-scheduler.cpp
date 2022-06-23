8238362859339a958a02dc68acaea39e1848fea7
weiyuhang1 <49320019+weiyuhang1@users.noreply.github.com>
2022-06-23 12:42:52 -0700

    Time: 402 ms (11.73%), Space: 40.3 MB (60.16%) - LeetHub

--- 1229-meeting-scheduler/1229-meeting-scheduler.cpp
+++ 1229-meeting-scheduler/1229-meeting-scheduler.cpp
@@ -4,12 +4,7 @@ public:
         sort(slots1.begin(), slots1.end(), comp);
         sort(slots2.begin(), slots2.end(), comp);
 
-        vector<int> result1 = helper(slots1, slots2, duration);
-        vector<int> result2;
-        result2 = helper(slots2, slots1, duration);
-        if (result1.empty()) return result2;
-        if (result2.empty()) return result1;
-        return result1[0] < result2[0] ? result1 : result2;
+        return helper(slots1, slots2, duration);
    //     
         
     }
@@ -22,21 +17,16 @@ private:
         const int m = slots1.size();
         const int n = slots2.size();
         for (int i = 0, j = 0; i < m && j < n; ) {
-            while (slots1[i][0] > slots2[j][0]) {
-                j++;
-                if (j >= n) return {};
-            }
-            if (slots2[j][0] + duration <= slots2[j][1] && slots2[j][0] + duration <= slots1[i][1]) {
-                return {slots2[j][0], slots2[j][0] + duration};
+            int start = max(slots1[i][0], slots2[j][0]);
+            int end = min(slots1[i][1], slots2[j][1]);
+            if (end - start >= duration) return {start, start + duration};
+            if (slots1[i][1] < slots2[j][1]) {
+                i++;
             } else {
-                if (j + 1 < n && slots2[j + 1][0] < slots1[i][1]) {
-                    j++;
-                } else {
-                    
-                    i++;
-                }
-                
+                j++;
             }
+            
+            
         }
         return {};
     }
