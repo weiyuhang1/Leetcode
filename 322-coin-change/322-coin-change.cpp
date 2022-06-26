453b8f04ba6d1ccc210e8384003d0bf88da41e03
weiyuhang1 <49320019+weiyuhang1@users.noreply.github.com>
2022-06-26 10:50:29 -0700

    Time: 444 ms (5.15%), Space: 63.7 MB (5.39%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -4,25 +4,23 @@ public:
         if (amount == 0) return 0;
         queue<int> q;
         unordered_set<int> dp;
-        for (int coin : coins) {
+        for (const int& coin : coins) {
                 q.push(coin);
 
             
         }
         int round = 1;
-        int minimum = *min_element(coins.begin(), coins.end());
 
-        while (!q.empty() && round <= amount/minimum + 1) {
+        while (!q.empty()) {
             int  sz = q.size();
             while (sz--) {
                 int val = q.front();
                 q.pop();
               
                 if (val == amount) return round;
-                if (val > amount) continue;
                 
                 for (int coin : coins) {
-                    if (coin <= amount - val && dp.find(coin + val) ==dp.end()) {
+                    if (coin <= amount - val && dp.find(coin + val) == dp.end()) {
                         q.push(coin + val);
                         dp.insert(coin + val);
                     }
