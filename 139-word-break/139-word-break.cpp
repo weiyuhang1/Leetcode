e7af061dd50f2459c6465f851b68e80112e5bf23
weiyuhang1 <49320019+weiyuhang1@users.noreply.github.com>
2022-06-27 11:34:13 -0700

    Time: 46 ms (16.36%), Space: 13.5 MB (32.11%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -5,16 +5,16 @@ public:
         unordered_set<string> dict(wordDict.begin(), wordDict.end());
         const int n = s.length();
         vector<bool> visited(n, false);
-        return bfs(s, dict, 0, n, visited);
+        return dfs(s, dict, 0, n, visited);
     }
 private:
-    bool bfs(string& s, unordered_set<string>& dict, int start, int len, vector<bool>& visited) {
+    bool dfs(string& s, unordered_set<string>& dict, int start, int len, vector<bool>& visited) {
         if (start == s.length()) return true;
         if (visited[start]) return false;
         for (int i = 1; i <= len; i++) {
             string str = s.substr(start, i);
             if (dict.find(str) != dict.end()) {
-                if (bfs(s, dict, start + i, len - i, visited)) return true;
+                if (dfs(s, dict, start + i, len - i, visited)) return true;
             } else {
                 continue;
             }
