14f9fe79f3a822b094a1743bc82f090ce34eeb4f
weiyuhang1 <49320019+weiyuhang1@users.noreply.github.com>
2022-06-28 12:24:14 -0700

    Time: 11 ms (86.92%), Space: 10.5 MB (96.10%) - LeetHub

--- 937-reorder-data-in-log-files/937-reorder-data-in-log-files.cpp
+++ 937-reorder-data-in-log-files/937-reorder-data-in-log-files.cpp
@@ -1,39 +1,36 @@
 class Solution {
 public:
     vector<string> reorderLogFiles(vector<string>& logs) {
-        stable_sort(logs.begin(), logs.end(), comp);
-        return logs;
-    }
-private:
-    static bool comp(string s1, string s2) {
-        istringstream ss(s1);
-        string word;
-        vector<string> v1;
-        while (ss >> word) {
-            v1.push_back(word);
-        }
-        
-        istringstream ss2(s2);
-        vector<string> v2;
-        while (ss2 >> word) {
-            v2.push_back(word);
-        }
-        
-        char c1 = v1[1][0];
-        char c2 = v2[1][0];
-        if (isdigit(c1) && isalpha(c2)) return false;
-        if (isalpha(c1) && isdigit(c2)) return true;
-        if (isalpha(c1) && isalpha(c2)) {
-            if (v1.size() == v2.size() && equal(v1.begin() + 1, v1.end(), v2.begin() + 1)) {
-                return v1[0] < v2[0];
+        vector<string> digits, result;
+        vector<pair<string, string>> letters;
+        for (string& s : logs) {
+            int i = 0;
+            while (s[i] != ' ') {
+                i++;
+            }
+            if (isalpha(s[i + 1])) {
+                letters.push_back({s.substr(0, i), s.substr(i + 1)});
             } else {
-                return lexicographical_compare(v1.begin() + 1, v1.end(), v2.begin() + 1, v2.end());
+                digits.push_back(s);
             }
         }
         
         
-        return false;
+        sort(letters.begin(), letters.end(), comp);
+        for (pair<string, string>& p : letters) {
+            result.push_back(p.first + " " + p.second);
+        }
+        for (string& s : digits) result.push_back(s);
+        return result;
     }
-    
+private:
+    static bool comp(pair<string, string>& s1, pair<string, string>& s2) {
+        if (s1.second == s2.second) {
+            return s1.first < s2.first;
+        } else {
+            return s1.second < s2.second;
+        }
+    }
+        
     
 };
