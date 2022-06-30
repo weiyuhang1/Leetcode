2420c732b6ec45885838759db26dbae6e8e7fc68
weiyuhang1 <49320019+weiyuhang1@users.noreply.github.com>
2022-06-29 22:53:47 -0700

    Time: 14 ms (69.51%), Space: 10.9 MB (15.75%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int minMoves2(vector<int>& nums) {
-        int average = nums[findKthSmallest(nums, nums.size() / 2, 0, nums.size() - 1)];
+        int average = quickselect(nums, nums.size() / 2, 0, nums.size() - 1);
         int dist = 0;
         for (auto num : nums) {
             dist += abs(num - average);
@@ -9,7 +9,21 @@ public:
         return dist;
     }
         
-     int partition(vector<int>& nums, int l, int r) {
+
+private:
+    int quickselect(vector<int>& nums, int k, int l, int r) {
+        if (l == r) return nums[l];
+        if (l > r) return -1000;
+        int x = partition(nums, l, r);
+        if (k == x) {
+            return nums[x];
+        } else if (k < x) {
+            return quickselect(nums, k, l, x - 1);
+        } else {
+            return quickselect(nums, k, x + 1, r);
+        }
+    }
+    int partition(vector<int>& nums, int l, int r) {
         int pivot = l + rand() % (r - l + 1);
         swap(nums[pivot], nums[r]);
         int j = l - 1;
@@ -21,19 +35,6 @@ public:
         }
         swap(nums[j + 1], nums[r]);
         return j + 1;
-    }
-    int findKthSmallest(vector<int>& nums, int k, int l, int r) {
-        if (l == r) return l;
-        if (l > r) return -1;
-        int pivot = partition(nums, l, r);
-        int len = pivot - l + 1;
-        if (pivot == k) {
-            return pivot;
-        } else if (pivot > k) {
-            return findKthSmallest(nums, k, l, pivot - 1);
-        } else {
-            return findKthSmallest(nums, k, pivot + 1, r);
-        }
     }
         
     
