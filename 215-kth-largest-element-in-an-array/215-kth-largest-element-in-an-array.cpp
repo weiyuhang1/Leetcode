927a5e9e4a8def2b8d22fa898c8d785bc2cf2f0b
weiyuhang1 <49320019+weiyuhang1@users.noreply.github.com>
2022-07-18 15:23:22 -0700

    Time: 99 ms (7.68%), Space: 45.5 MB (8.87%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -6,30 +6,28 @@ public:
 
     int findKthSmallest(vector<int>& nums, int k, int l, int r) {
         if (l == r) return l;
-        if (l > r) return -1;
         int pivot = partition(nums, l, r);
-        int len = pivot - l + 1;
         if (pivot == k) {
             return pivot;
         } else if (pivot > k) {
             return findKthSmallest(nums, k, l, pivot - 1);
         } else {
-            return findKthSmallest(nums, k, pivot + 1, r);  //k is an index here
+            return findKthSmallest(nums, k, pivot + 1, r);
         }
     }
-        int partition(vector<int>& nums, int p, int r) {
-        int q = p + rand() % (r - p + 1);  //here q is an index not the kth element after p
+    int partition(vector<int>& nums, int p, int r) {
+        int q = p + rand() % (r - p + 1);
         swap(nums[q], nums[r]);
         int i = p - 1;
         for (int j = p; j < r; j++) {
-            if (nums[j] <= nums[r]) {
+            if (nums[j] < nums[r]) {
                 i++;
-                swap(nums[i], nums[j]);
+                swap(nums[j], nums[i]);
             }
         }
-        swap(nums[i + 1], nums[r]);
-        return i + 1;
+        i++;
+        swap(nums[i], nums[r]);
+        return i;
     }
-    
     
 };
