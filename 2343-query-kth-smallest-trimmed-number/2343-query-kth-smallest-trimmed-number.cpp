12983edcb31a783bef59fac0c3ff4ff289bb34fd
weiyuhang1 <49320019+weiyuhang1@users.noreply.github.com>
2022-07-18 17:55:58 -0700

    Time: 384 ms (55.56%), Space: 34.5 MB (77.78%) - LeetHub

--- 2343-query-kth-smallest-trimmed-number/2343-query-kth-smallest-trimmed-number.cpp
+++ 2343-query-kth-smallest-trimmed-number/2343-query-kth-smallest-trimmed-number.cpp
@@ -1,19 +1,60 @@
 class Solution {
 public:
+
     vector<int> smallestTrimmedNumbers(vector<string>& nums, vector<vector<int>>& queries) {
-    int n = nums[0].size();
-    vector<pair<string, int>> pairs;
-    for (int i = 0; i < nums.size(); ++i)
-        pairs.push_back({ nums[i], i });
-    vector<int> res;
-    for(auto &q : queries) {
-        int k = q[0], trim = q[1];
-        nth_element(begin(pairs), begin(pairs) + k - 1, end(pairs), [&](const auto &a, const auto &b){
-            int cmp = a.first.compare(n - trim, string::npos, b.first, n - trim, string::npos);
-            return cmp == 0 ? a.second < b.second : cmp < 0;
-        });
-        res.push_back(pairs[k - 1].second);
+        vector<int> result;
+        int len = nums[0].length();
+        vector<pair<string, int>> nums1;
+        nums1.clear();
+            
+            for (int i = 0; i < nums.size(); i++) {
+                nums1.push_back({nums[i], i});
+            }
+        for (auto query : queries) {
+            int k = query[0];
+            int trim = query[1];
+
+            nth_element(nums1.begin(), nums1.begin() + k - 1, nums1.end(), [&](const auto& a, const auto& b) {
+                int cmp = a.first.compare(len - trim, string :: npos, b.first, len - trim, string :: npos);
+                if (cmp == 0) {
+                    return a.second < b.second;
+                } else {
+                    return cmp < 0;
+                }
+            });
+            result.push_back(nums1[k - 1].second);
+       //     result.push_back(findKthSmallest(nums1, query[0], 0, nums1.size() - 1));
+        }
+        return result;
+        
     }
-    return res;}
+
+    int partition(vector<string>& nums, int p, int r) {
+        int q = p + rand() % (r - p + 1);
+        swap(nums[q], nums[r]);
+        int i = p - 1;
+        for (int j = p; j < r; j++) {
+            if (nums[j] < nums[r]) {
+                i++;
+                swap(nums[i], nums[j]);
+            }
+        }
+        swap(nums[i + 1], nums[r]);
+        return i + 1;
+    }
+    int findKthSmallest(vector<string>& nums, int k, int l, int r) {
+        if (l == r) return l;
+        if (l > r) return -1;
+        int pivot = partition(nums, l, r);
+        int len = pivot - l + 1;
+        if (pivot == k) {
+            return pivot;
+        } else if (pivot > k) {
+            return findKthSmallest(nums, k, l, pivot - 1);
+        } else {
+            return findKthSmallest(nums, k, pivot + 1, r);
+        }
+    }
+    
     
 };
